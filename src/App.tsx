5adb55cace24b60528c5c929c3d0d00558ebf6d5
shalun11 <git@stackblitz.com>
2026-05-27 21:27:53 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return React.createElement(
+    "h3",
+    null,
+    "Никогда не сдавайтесь, идите к своей цели! А если будет сложно – сдавайтесь. ©"
+  );
+}
+
+export default App;
